74787929f37d2cff5709a9b18eadd9e23a7dc14c
ToasT1ng <jrainboww9@gmail.com>
2020-12-29 18:18:37 +0900

    ManyToOne & OneToMany Complete

--- jpatest/src/main/java/jpatest/start/JpaMain.java
+++ jpatest/src/main/java/jpatest/start/JpaMain.java
@@ -37,6 +37,8 @@ public class JpaMain {
 		Member insertMember = new Member();
 		Member insertMember2 = new Member();
 		Order insertOrder = new Order();
+		Item insertItem = new Item();
+		OrderItem insertOrderItem = new OrderItem();
 		
 		
 		insertMember.setCity("seoul");
@@ -56,20 +58,34 @@ public class JpaMain {
 		Member findMember = em.find(Member.class, Long.valueOf(2));
 		System.out.println("findMember=" + findMember.getName());
 		
-		
 		insertOrder.setMember(insertMember);
 		insertOrder.setOrderDate(new Date());
 		insertOrder.setStatus(OrderStatus.ORDER);
 		
 		em.persist(insertOrder);
 		
+		insertItem.setName("thisIsItem");
+		insertItem.setPrice(1000);
+		insertItem.setStockQuantity(3);
+		
+		em.persist(insertItem);
+		
+		insertOrderItem.setItem(insertItem);
+		insertOrderItem.setCount(5);
+		insertOrderItem.setOrder(insertOrder);
+		insertOrderItem.setOrderPrice(3000);
+		insertOrder.addOrderItem(insertOrderItem);
+		
+		em.persist(insertOrderItem);
+		
 		Order order1 = em.find(Order.class, Long.valueOf(3));
 		Member member1 = order1.getMember();
 		System.out.println("findMember=" + member1.getName());
-//		
-//		Order order2 = em.find(Order.class, "orderId_2");
-//		OrderItem orderItem = order2.getOrderItems().get(0);
-//		Item item = orderItem.getItem();
+
+		Order order2 = em.find(Order.class, Long.valueOf(3));
+		OrderItem orderItem = order2.getOrderItems().get(0);
+		Item item = orderItem.getItem();
+		System.out.println("findItem=" + item.getName());
 		
 		
 	}
